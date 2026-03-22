a70828c350af5c652ccd8d8dcd969ef957efec89
miksnmatch <miko.paalan@gmail.com>
2026-03-22 16:19:05 +0800

    Add URL patterns for all views

--- config/urls.py
+++ config/urls.py
@@ -1,22 +1,30 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from taskmanager import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('', views.TaskListView.as_view(), name='home'),
+    path('tasks/', views.TaskListView.as_view(), name='task-list'),
+    path('tasks/add/', views.TaskCreateView.as_view(), name='task-add'),
+    path('tasks/<int:pk>/', views.TaskDetailView.as_view(), name='task-detail'),
+    path('tasks/<int:pk>/edit/', views.TaskUpdateView.as_view(), name='task-edit'),
+    path('tasks/<int:pk>/delete/', views.TaskDeleteView.as_view(), name='task-delete'),
+    path('subtasks/', views.SubTaskListView.as_view(), name='subtask-list'),
+    path('subtasks/add/', views.SubTaskCreateView.as_view(), name='subtask-add'),
+    path('subtasks/<int:pk>/edit/', views.SubTaskUpdateView.as_view(), name='subtask-edit'),
+    path('subtasks/<int:pk>/delete/', views.SubTaskDeleteView.as_view(), name='subtask-delete'),
+    path('notes/', views.NoteListView.as_view(), name='note-list'),
+    path('notes/add/', views.NoteCreateView.as_view(), name='note-add'),
+    path('notes/<int:pk>/edit/', views.NoteUpdateView.as_view(), name='note-edit'),
+    path('notes/<int:pk>/delete/', views.NoteDeleteView.as_view(), name='note-delete'),
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/add/', views.CategoryCreateView.as_view(), name='category-add'),
+    path('categories/<int:pk>/edit/', views.CategoryUpdateView.as_view(), name='category-edit'),
+    path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category-delete'),
+    path('priorities/', views.PriorityListView.as_view(), name='priority-list'),
+    path('priorities/add/', views.PriorityCreateView.as_view(), name='priority-add'),
+    path('priorities/<int:pk>/edit/', views.PriorityUpdateView.as_view(), name='priority-edit'),
+    path('priorities/<int:pk>/delete/', views.PriorityDeleteView.as_view(), name='priority-delete'),
 ]
